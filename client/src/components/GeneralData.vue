8fb6a83b17a2a88be4e0f6a3de89fca7bf1c9d4c
sanavila <rute.leone@hotmail.com>
2022-12-27 21:46:46 -0300

    implementação do field de entrada de foto d perfil

--- client/src/components/GeneralData.vue
+++ client/src/components/GeneralData.vue
@@ -1,14 +1,13 @@
 <template>
   <section>
     <div class="columns py-5 mb-0 is-align-items-center hr">
-      <span class="column is-3 has-text-left">Nome / Sobrenome</span>
-      <b-field
-        class="column is-6">
+      <span class="column is-3 has-text-left has-text-weight-semibold">Nome / Sobrenome</span>
+      <b-field class="column is-6">
         <b-input value="Kevin Garvey"></b-input>
       </b-field>
     </div>
     <div class="columns py-5 mb-0 is-align-items-center hr">
-      <span class="column is-3 has-text-left">Nascimento / Sexo</span>
+      <span class="column is-3 has-text-left has-text-weight-semibold">Nascimento / Sexo</span>
       <b-field class="column is-3 mb-0">
         <b-datepicker placeholder="Clique para selecionar uma data" trap-focus>
         </b-datepicker>
@@ -22,19 +21,52 @@
         </b-select>
       </b-field>
     </div>
-
-    <b-field label="Username" type="is-success" message="This username is available" horizontal>
-      <b-input value="johnsilver" maxlength="30"></b-input>
-    </b-field>
-
-    <b-field label="Password" horizontal>
-      <b-input type="password" value="iwantmytreasure" password-reveal>
-      </b-input>
-    </b-field>
-
-    <b-field label="Message" horizontal>
-      <b-input maxlength="200" type="textarea"></b-input>
-    </b-field>
+    <div class="columns py-5 mb-0 hr">
+      <div class="column is-3 has-text-left">
+        <span class="has-text-weight-semibold">Sua foto</span>
+        <p>Isso será exibido no seu perfil.</p>
+      </div>
+      <img class="column is-1" :src="imageURL" alt="">
+      <b-field class="column is-5 custom-w">
+        <b-upload v-model="dropFiles" drag-drop accept=".svg, .png, .jpg">
+          <section class="section p-1">
+            <div class="content has-text-centered">
+              <p>
+                <b-icon icon="cloud-upload" size="is-large">
+                </b-icon>
+              </p>
+              <p>Clique para fazer upload ou arraste e solte</p>
+              <p>SVG, PNG ou JPG(máximo de 800x800)</p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
+    </div>
+    <div class="columns py-5 mb-0 hr">
+      <span class="column is-3 has-text-left has-text-weight-semibold">E-mail / Telefone</span>
+      <b-field class="column is-3">
+        <b-input placeholder="Email"
+          v-model="email"
+          type="email"
+          icon="email">
+        </b-input>
+      </b-field>
+      <b-field class="column is-3">
+        <b-input placeholder="Telefone" icon="phone-outline"></b-input>
+      </b-field>
+    </div>
+    <div class="columns py-5 mb-0 hr">
+      <span class="column is-3 has-text-left has-text-weight-semibold">RG / CPF / CNS</span>
+      <b-field class="column is-2">
+        <b-input placeholder="00000000000"></b-input>
+      </b-field>
+      <b-field class="column is-2">
+        <b-input placeholder="00000000000"></b-input>
+      </b-field>
+      <b-field class="column is-2">
+        <b-input placeholder="00000000000"></b-input>
+      </b-field>
+    </div>
   </section>
 </template>
 
@@ -42,13 +74,14 @@
 export default {
   name: 'GeneralData',
   data() {
-    const today = new Date();
-
     return {
-      date: new Date(),
-      minDate: new Date(today.getFullYear() - 80, today.getMonth(), today.getDate()),
-      maxDate: new Date(today.getFullYear() + 18, today.getMonth(), today.getDate()),
+      dropFiles: null,
     };
+  },
+  computed: {
+    imageURL() {
+      return this.dropFiles ? URL.createObjectURL(this.dropFiles) : '';
+    },
   },
 };
 </script>
@@ -56,6 +89,7 @@ export default {
 .hr {
   border-top: 1px solid #dfdfdf;
 }
+
 .container-one {
   max-width: 80%;
 }
